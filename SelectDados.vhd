4fce6bd470f81f21d637fc645f199b05c77a7f44
estrelii <96571158+estrelii@users.noreply.github.com>
2022-12-15 19:51:19 +0100

    Version Final

--- SelectDados.vhd
+++ SelectDados.vhd
@@ -15,7 +15,6 @@ end SelectDados;
 architecture Behavioral of SelectDados is
 
 signal dado_i   : std_logic_vector(17 downto 0);
-signal dado_sel : std_logic_vector(17 downto 0);
 
 begin
     -- process(clk, reset, sw, dado) -- ¿esta bien esta lista de sensibilidad?
@@ -25,23 +24,31 @@ begin
             dado_i <= (others => '0');
         elsif(clk'event and clk = '1') then 
             if sel = '1' then
-                if sw = "-----1" then
+                if sw(0)='1' then
                     dado_i(2 downto 0) <= dado(2 downto 0);
-                if sw = "----1-" then
+                end if;
+                if sw(1)='1' then
                     dado_i(5 downto 3) <= dado(5 downto 3);
-                if sw = "---1--" then
+                end if;
+                if sw(2)='1' then
                     dado_i(8 downto 6) <= dado(8 downto 6);
-                if sw = "--1---" then
+                end if;                
+                if sw(3)='1' then
                     dado_i(11 downto 9) <= dado(11 downto 9);
-                if sw = "-1----" then
+                end if;
+                if sw(4)='1' then
                     dado_i(14 downto 12) <= dado(14 downto 12);
-                if sw =  "1-----" then
+                end if;
+                if sw(5)='1' then
                     dado_i(17 downto 15) <= dado(17 downto 15);
+                end if;
                 if sw = "000000" then
                     dado_i <= (others => '0');
-                end case;
+                end if;
+                
             else 
                 dado_i <= (others => '0');
+            
             end if;
         end if;  
     end process;
