78d30a61366fb60145a4fa84c393bf4b59c1df84
chema <chemabc@gmail.com>
2022-12-15 17:43:09 +0100

    well done? (lista de sensibilidad)

--- SelectDados.vhd
+++ SelectDados.vhd
@@ -14,36 +14,37 @@ end SelectDados;
 
 architecture Behavioral of SelectDados is
 
-signal dado_i   : std_logic_vector(17 downto 0);
+signal dado_i : std_logic_vector(17 downto 0);
 
 begin
-process(clk,reset,sw,dado)
-begin 
-    if reset='1' then 
-        dado_i<=(others=>'0');
-    elsif(clk'event and clk='1') then 
-        if sel='1' then 
-            case sw is
-                when "-----1" =>
-                    dado_i(2 downto 0)<=dado(2 downto 0);
-                when "----1-" =>
-                    dado_i(5 downto 3)<=dado(5 downto 3);
-                when "---1--" =>
-                    dado_i(8 downto 6)<=dado(8 downto 6);
-                when "--1---" =>
-                    dado_i(11 downto 9)<=dado(11 downto 9);
-                when "-1----" =>
-                    dado_i(14 downto 12)<=dado(14 downto 12);
-                when "1-----" =>
-                when "000000" =>
-                    dado_i<=(others=>'0');
-            end case;
-        else 
-          dado_i<=(others=>'0');
-      end if;
-    end if;  
-end process;
-dado_sel<=dado_i;
+    process(clk, reset, sw, dado) -- ¿esta bien esta lista de sensibilidad?
+    begin 
+        if reset='1' then 
+            dado_i <= (others => '0');
+        elsif(clk'event and clk='1') then 
+            if sel='1' then 
+                case sw is
+                    when "-----1" =>
+                        dado_i(2 downto 0) <= dado(2 downto 0);
+                    when "----1-" =>
+                        dado_i(5 downto 3) <= dado(5 downto 3);
+                    when "---1--" =>
+                        dado_i(8 downto 6) <= dado(8 downto 6);
+                    when "--1---" =>
+                        dado_i(11 downto 9) <= dado(11 downto 9);
+                    when "-1----" =>
+                        dado_i(14 downto 12) <= dado(14 downto 12);
+                    when "1-----" =>
+                    when "000000" =>
+                        dado_i <= (others => '0');
+                end case;
+            else 
+                dado_i <= (others => '0');
+            end if;
+        end if;  
+    end process;
+
+    dado_sel <= dado_i;
 
  
 
