83fd2a3922e55492f10a292227cde883997d0359
chema <chemabc@gmail.com>
2022-12-16 16:42:12 +0100

    SelectDados cleaned

--- SelectDados.vhd
+++ SelectDados.vhd
@@ -17,35 +17,33 @@ architecture Behavioral of SelectDados is
 signal dado_i   : std_logic_vector(17 downto 0);
 
 begin
-    -- process(clk, reset, sw, dado) -- ¿esta bien esta lista de sensibilidad?
     process(clk, reset)
     begin 
         if reset = '1' then 
             dado_i <= (others => '0');
         elsif(clk'event and clk = '1') then 
             if sel = '1' then
-                if sw(0)='1' then
+                if sw(0) = '1' then
                     dado_i(2 downto 0) <= dado(2 downto 0);
                 end if;
-                if sw(1)='1' then
+                if sw(1) = '1' then
                     dado_i(5 downto 3) <= dado(5 downto 3);
                 end if;
-                if sw(2)='1' then
+                if sw(2) = '1' then
                     dado_i(8 downto 6) <= dado(8 downto 6);
                 end if;                
-                if sw(3)='1' then
+                if sw(3) = '1' then
                     dado_i(11 downto 9) <= dado(11 downto 9);
                 end if;
-                if sw(4)='1' then
+                if sw(4) = '1' then
                     dado_i(14 downto 12) <= dado(14 downto 12);
                 end if;
-                if sw(5)='1' then
+                if sw(5) = '1' then
                     dado_i(17 downto 15) <= dado(17 downto 15);
                 end if;
                 if sw = "000000" then
                     dado_i <= (others => '0');
                 end if;
-                
             else 
                 dado_i <= (others => '0');
             
