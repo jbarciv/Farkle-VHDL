616d1396240d3f9b113fb94c0e7be2f286108251
chema <chemabc@gmail.com>
2022-12-16 18:39:09 +0100

    cleaner

--- Randomtb.vhd
+++ Randomtb.vhd
@@ -1,51 +1,19 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 10.11.2022 19:28:16
--- Design Name: 
--- Module Name: Randomtb - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity Randomtb is
-
 end Randomtb;
-
 
 architecture Behavioral of Randomtb is
 component Random is
 Port (clk : in std_logic;
             reset : in std_logic;
             tirar : in std_logic
-             );
+        );
 end component;
 
-signal clk_tb,reset_tb,tirar_tb : std_logic;
-signal dado1,dado2,dado3,dado4,dado5,dado6: integer;
+signal clk_tb, reset_tb, tirar_tb : std_logic;
+signal dado1, dado2, dado3, dado4, dado5, dado6: integer;
 signal cnt_rand : integer;
 constant clk_period : time := 8 ns;
 
