0f8ddcd304748d49e3d1986af44ab3a53c35c2d8
TomasNebot <tomas.nebot@gmail.com>
2022-12-24 21:38:29 +0100

    Co-authored-by: manuiglesias8 <manuiglesias8@users.noreply.github.com>

--- Controlador.vhd
+++ Controlador.vhd
@@ -19,16 +19,30 @@ end Controlador;
 architecture Behavioral of Controlador is
 
     --FSM
-    type state_t is (s_reset, s_tirar, s_ptos, s_farkle, s_win);
-    signal state : state_t;
+    type estados is (S_ESPERAR, S_MOSTRAR, S_FARKLE, S_CALCULA, S_INVALIDO,S_MOSTRAR_PTOS, S_WIN);
+    signal estado : estados;
     
     --Señales para activar cada bloque
-    signal en_tirar : std_logic;
-    signal en_farkle : std_logic;
+    signal en_lfsr_top : std_logic;
+    signal en_comprobar_farkle : std_logic;
+    signal en_mostrar_dados : std_logic;
+    signal en_mostrar_error : std_logic;
+    signal en_mostrar_ptos : std_logic;
+    signal en_farkle_ok : std_logic;
+    signal en_player : std_logic; --Cambia de jugador
     signal en_win : std_logic;
-    
-    
+    signal en_calcula : std_logic;
+    signal en_ptos_ronda : std_logic;
+    signal en_ptos_partida : std_logic;
 
+    signal flag_sel : std_logic;
+
+    -- Señales ready
+    signal ready_error : std_logic;
+    signal ready_calcula : std_logic;
+    signal ready_mostrar_ptos : std_logic;
+    signal ready_win : std_logic;
+    
 begin
 
 --Aqui irian todos los componentes que usaremos con la inst(work."nombre del bloque")
@@ -38,31 +52,87 @@ begin
 process(clk,reset)
 begin
     if(reset = '1') then
-        state <= s_reset;
+        estado <= S_ESPERAR;
     elsif(clk'event and clk='1') then
-        case state is
-            when s_reset =>
-                if(tirar = '1') then
-                    state <= s_tirar;
+        case estado is
+            when S_ESPERAR =>
+                en_player <= '0';
+                if(tirar='1') then
+                    en_lfsr_top <= '1';
+                    en_comprobar_farkle <= '1';
+                    estado <= S_MOSTRAR;
                 end if;
-            when s_tirar =>
-                if(en_farkle = '1') then
-                    state <= s_farkle;
-                elsif(sel = '1' or planta = '1') then
-                    state <= s_ptos;
+                
+            when S_MOSTRAR =>
+                en_lfsr_top <='0';
+                en_mostrar_dados <= '1';
+                en_comprobar_farkle <= '1';
+
+                if (farkle_ok='1') then
+                    estado <= S_FARKLE;
+                    en_farkle_ok <='1';
+                    
+                elsif sel='1' or planta='1' then
+                    en_calcula <= '1';
+                    estado <= S_CALCULA;
+                    if sel='1' then
+                        flag_sel <= '1';
+                    else
+                        flag_sel <= '0';
+                    end if;    
                 end if;
-            when s_ptos =>
-                if(en_tirar = '1') then
-                    state <= s_tirar;
-                elsif(en_win = '1') then
-                    state <= s_win;
+
+            when S_FARKLE =>
+                en_player <= '1';
+                estado <= S_ESPERAR;
+
+            when S_CALCULA =>
+
+                if(en_error = '1') then
+                    estado <= S_INVALIDO;
+                    en_mostrar_dados <= '0';
+                    en_mostrar_error <='1';
+                elsif (ready_calcula='1')
+                    estado <= S_MOSTRAR_PTOS;
+                    en_mostrar_dados <= '0';
+                    en_mostrar_ptos <= '1';
+                 end if;   
+                
+            when S_INVALIDO =>
+                en_mostrar_error <='0';
+                if(ready_error = '1') then
+                    estado <= S_MOSTRAR;
                 end if;
-            when s_farkle =>
-                if(en_tirar = '1') then
-                    state <= s_tirar;
+                
+            when S_MOSTRAR_PTOS =>
+                
+                if(flag_sel='1') then
+                    en_ptos_ronda <= '1';
+                    en_ptos_partida <= '0';
+                elsif(flag_sel='0') then
+                    en_ptos_ronda <= '1';
+                    en_ptos_partida <= '1';
                 end if;
-            when s_win =>
-            end case;
+
+                if (ready_mostrar_ptos = '1' and ready_win = '1') then
+                    en_ptos_ronda <= '0';
+                    en_ptos_partida <= '0';
+                    en_mostrar_ptos <= '0';
+                    estado <= S_WIN;
+                end if;
+                
+                if(ready_mostrar_ptos = '1' and ready_win = '0') then
+                    en_ptos_ronda <= '0';
+                    en_ptos_partida <= '0';
+                    en_mostrar_ptos <= '0';
+                    estado <= S_ESPERAR;
+                end if;
+                    
+            when S_WIN =>
+                -- Acciones a realizar en el estado S_WIN
+            when others =>
+                -- Acciones a realizar en caso de que no se cumpla ninguna de las condiciones anteriores
+        end case;
     end if;  
 end process;
      
