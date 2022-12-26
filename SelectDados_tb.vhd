3b4d163978ab97710521983b80b2791fff127e7e
estrelii <96571158+estrelii@users.noreply.github.com>
2022-12-26 14:03:41 +0100

    Definitivo

--- SelectDados_tb.vhd
+++ SelectDados_tb.vhd
@@ -5,41 +5,47 @@ USE ieee.std_logic_1164.ALL;
 ENTITY SelectDados_tb IS
 END SelectDados_tb;
  
-ARCHITECTURE behavior OF SelectDados_tb IS 
+ARCHITECTURE behavioral OF SelectDados_tb IS 
  
     -- Component Declaration for the Unit Under Test (UUT)
-    COMPONENT SelectDados is
-		Port (clk     : in std_logic;
-			  reset   : in std_logic;
-			  sel     : in std_logic;
-			  sw      : in std_logic_vector(5 downto 0);
-			  dado    : in std_logic_vector(17 downto 0);
-			  dado_sel: out std_logic_vector(17 downto 0)
-			  );
-    END COMPONENT;
+component  SelectDados_v1 is
+  Port (clk     : in std_logic;
+        reset   : in std_logic;
+        sel     : in std_logic;
+        sw      : in std_logic_vector(5 downto 0);
+        dados   : in std_logic_vector(17 downto 0);
+        dado_pto: out std_logic_vector(2 downto 0);
+        dado_valido: out std_logic
+        );
+end component;
     
    --Inputs
    signal clk	: std_logic := '0';
    signal sel	: std_logic := '0';
    signal sw	: std_logic_vector(5 downto 0) := "000000";
-   signal dado 	: in std_logic_vector(17 downto 0) := "011011001110100011"; -- dado = 33164  
+   signal dados 	: std_logic_vector(17 downto 0) := "011011001110100011"; -- dado = 331643  
+   signal reset : std_logic;
+   
 
  	--Outputs
-   signal dado_sel : std_logic_vector(17 downto 0);
+    signal dado_pto: std_logic_vector(2 downto 0);
+    signal dado_valido: std_logic;
 
    -- Clock period definitions
    constant clk_period : time := 8 ns;
+  
  
 BEGIN
  
 	-- Instantiate the Unit Under Test (UUT)
-   uut: SelectDados PORT MAP (
+   uut: SelectDados_v1 PORT MAP (
           	clk 	=> clk,
 			reset 	=> reset,
           	sel 	=> sel,
           	sw 		=> sw,
-			dado 	=> dado,
-			dado_sel => dado_sel
+			dados   => dados, 
+            dado_pto=> dado_pto,
+            dado_valido=>dado_valido
    		);
 
    -- Clock process definitions
@@ -60,15 +66,15 @@ BEGIN
 		sel <= '0';
 		wait for 10*clk_period;
 		reset <= '0';
-		wait for 1*clk_period;
+		wait for 10*clk_period;
 	 	
 		sw <= "111001";
 		sel <= '1';
-		wait for 10*clk_period;
-		sel <= '0';
 		wait for 10*clk_period;
       wait;
 		
    end process;
 
-END;
+
+
+end Behavioral;
