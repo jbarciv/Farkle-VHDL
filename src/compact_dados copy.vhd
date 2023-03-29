075a773a79e6a565c06b8c28c9f6f02d603080c0
chema <chemabc@gmail.com>
2023-03-29 12:50:57 +0200

    copy actualizado

--- src/compact_dados copy.vhd
+++ src/compact_dados copy.vhd
@@ -2,38 +2,24 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
--- AUN NO ESTA ACABADO... PERO ESTE FINDE INTENTO HACER UN TEST-BENCH PARA COMPROBAR SI FUNCIONA
--- BÁSICAMENTE: DADO UN NUMERO ALEATORIO (17 DOWNTO 0) Y LOS SWITCHES QUE SE HAN BAJADO DEVUELVE EL NUMERO
--- DE NUMEROS QUE DEBEN MOSTRARSE Y UN VECTOR CON LOS NUMERO A MOSTRAR JUNTOS EN LAS POSICIONES MÁS ALTAS DEL VECTOR
--- Y SE HA AÑADIDO JUSTO AL FINAL EL NUMERO '111' = 7 PARA LUEGO CONVERTIRL0 EN EL SEPARADOR.
-
-entity compact_dados is
-  Port (clk             : in std_logic;
-        reset           : in std_logic;
-        en_compacta     : in std_logic;
-        sw              : in std_logic_vector(5 downto 0);
-        dados           : in std_logic_vector(17 downto 0);
-        ready_compacta  : out std_logic;
-        num_dados       : out std_logic_vector(2 downto 0);
-        dados_s         : out std_logic_vector(20 downto 0)
+entity compacta_dados is
+  Port (clk                 : in std_logic;
+        reset               : in std_logic;
+        en_compacta         : in std_logic;
+        dados               : in std_logic_vector(17 downto 0);
+        num_dados_mostrar   : in std_logic_vector(2 downto 0);
+        ready_compacta      : out std_logic;
+        dados_s             : out std_logic_vector(20 downto 0)
         );
-end compact_dados;
+end compacta_dados;
 
-architecture Behavioral of compact_dados is
+architecture Behavioral of compacta_dados is
 
 -- Maquina de estados
 type Status_t is (S_ESPERANDO, S_CUENTA, S_COMPACTANDO, S_COMPACTADO);
 signal STATE: Status_t;
 
-
-signal en_aux_cuenta        : std_logic;
-signal flag_aux_cuenta      : std_logic;
-signal en_aux               : std_logic;
-signal flag_aux             : std_logic;
-
-signal num_dados_validos    : integer range 6 to 0;
-signal n_to_show            : integer range 6 to 0;
-
+signal flag_aux : std_logic;
 
 begin
 
@@ -46,18 +32,11 @@ begin
             
                 when S_ESPERANDO =>
                     if 	( en_compacta = '1') then   
-                        STATE <= S_CUENTA;  
-
-                when S_CUENTA =>
-                    en_aux_cuenta <= '1';
-                    if (flag_aux_cuenta = '1') then
-                        en_aux_cuenta <= '0';
-                        STATE <= S_COMPACTANDO;
+                        STATE <= S_COMPACTANDO;  
+                    end if;
 
                 when S_COMPACTANDO =>
-                        en_aux <= '1';
-                    if 	( flag_aux = '1') then
-                        en_aux <= '0';
+                    if 	( flag_aux = '1' ) then
                         ready_compacta <= '1';
                         STATE <= S_COMPACTADO;
                     end if;
@@ -71,52 +50,43 @@ begin
         end if;
     end process;
 
-    -- Cuento cuantos '1's hay en sw: cuantos dados se retiran
-    process (clk, reset)
-    begin
-        if (reset = '1') then
-            num_dados_validos <= 0;
-            flag_aux_cuenta = '0';
-            n_to_show <= 0;
-        elsif (clk'event and clk = '1') then
-            if (en_aux_cuenta = '1' and flag_aux_cuenta = '0') then
-                for i in 0 to 5 loop
-                    if (sw(i) = '1') then
-                        num_dados_validos <= num_dados_validos + 1;
-                    end if;            
-                end loop;
-                n_to_show <= n_to_show - num_dados_validos;
-                flag_aux_cuenta <= '1';
-            elsif (STATE = S_COMPACTANDO) then
-                flag_aux_cuenta <= '0';
-                num_dados_validos <= 0;
-            end if;
-        end if;
-    end process;
+    -- -- Cuento cuantos '1's hay en sw: cuantos dados se retiran
+    -- process (clk, reset)
+    -- begin
+    --     if (reset = '1') then
+    --         num_dados_validos <= 0;
+    --         flag_aux_cuenta <= '0';
+    --         n_to_show <= 0;
+    --     elsif (clk'event and clk = '1') then
+    --         if (en_aux_cuenta = '1' and flag_aux_cuenta = '0') then
+    --             for i in 0 to 5 loop
+    --                 if (sw(i) = '1') then
+    --                     num_dados_validos <= num_dados_validos + 1;
+    --                 end if;            
+    --             end loop;
+    --             n_to_show <= n_to_show - num_dados_validos;
+    --             flag_aux_cuenta <= '1';
+    --         elsif (STATE = S_COMPACTANDO) then
+    --             flag_aux_cuenta <= '0';
+    --             num_dados_validos <= 0;
+    --         end if;
+    --     end if;
+    -- end process;
 
-    -- Compacto segun los '1's de sw y los numeros a mostrar previos
+    -- Compacto segun los numeros a mostrar (que me ha dicho la M. Estados)
     process (clk, reset)
     begin
         if (reset = '1') then
-            dados_
-            flag_aux <= 0;
+            flag_aux <= '0';
         elsif (clk'event and clk = '1') then
-            if (en_aux = '1' and flag_aux = '0') then
-                for i in 5 to 0 loop
-                    if (sw(i) = '0') then
-                        dados_s((3*i+2) downto (3*i)) <= dados((3*i+2) downto (3*i));
-                end loop;
-                dados_s((20-3*n_to_show) downto (20-3*n_to_show-2)) <= '111';
+            if (en_compacta = '1' and flag_aux = '0') then
+                dados_s((20-3*TO_INTEGER(UNSIGNED(num_dados_mostrar))) downto (20-3*TO_INTEGER(UNSIGNED(num_dados_mostrar))-2)) <= "111";
                 flag_aux <= '1';
             elsif (STATE = S_COMPACTADO) then
                 flag_aux <= '0';
             end if;
         end if;
     end process;
-
-
-            
-            
-
     
 end Behavioral;
+
--- src/scroll.vhd
+++ src/scroll.vhd
@@ -3,22 +3,21 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
 entity scroll is
-    Port (  clk : in std_logic;
-            reset : in std_logic;
-            dados : in std_logic_vector(17 downto 0);
-            enable_1s : in std_logic;
-            en_refresh : in std_logic;
-            dados_s : out std_logic_vector(20 downto 0)
+    Port (  clk         : in std_logic;
+            reset       : in std_logic;
+            dados       : in std_logic_vector(17 downto 0);
+            enable_1s   : in std_logic;
+            en_refresh  : in std_logic;
+            dados_s     : out std_logic_vector(20 downto 0)
     );
 end scroll;
 
 architecture Behavioral of scroll is
 
--- Señales dados
+-- Senales dados
     signal dados_i: std_logic_vector(20 downto 0);
 
 begin
-
 
 -- Desplazamiento
     process(clk, reset)
@@ -27,7 +26,7 @@ begin
             dados_i <= (others => '0');
         elsif(clk'event and clk = '1') then
             if(en_refresh = '1') then
-                dados_i <= dados(17 downto 0) & "111";--Vector Manu
+                dados_i <= dados(17 downto 0) & "111";
             end if;
             if(enable_1s = '1') then
                 dados_i(20 downto 0) <= dados_i(17 downto 0) & dados_i(20 downto 18);
