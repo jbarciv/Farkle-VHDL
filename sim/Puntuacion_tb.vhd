ef395a7ba6a0fd2cf5dfd7f396208d931dfc9578
estrelii <96571158+estrelii@users.noreply.github.com>
2023-04-27 17:38:47 +0200

    puntuacion_tb 2704

--- sim/Puntuacion_tb.vhd
+++ sim/Puntuacion_tb.vhd
@@ -7,23 +7,22 @@ entity Puntuacion_tb is
 end Puntuacion_tb;
 
 architecture Behavioral of Puntuacion_tb is
-component Puntuacion is
-  Port (clk             : in std_logic; 
-        reset           : in std_logic;
-        en_calcula      : in std_logic;
-        dado_pto        : in std_logic_vector(2 downto 0);
-        en_comprobar_farkle: in std_logic;
-        ptos            : out std_logic_vector(13 downto 0);
-        error           : out std_logic;
-        ready_puntuacion: out std_logic; 
-        farkle_ok       : out std_logic
+component Puntuacion is --decirle a chema cuantos dados tiene que mostrar en la siguiente tirada
+  Port (clk                 : in std_logic;
+        reset               : in std_logic;
+        en_calcula          : in std_logic;
+        dados               : in std_logic_vector(17 downto 0);
+        ptos                : out std_logic_vector(13 downto 0);
+        error               : out std_logic;
+        ready_puntuacion    : out std_logic;
+        farkle_ok           : out std_logic 
         );
 end component;
 
 
 --Inputs
-signal clk, reset, en_calcula, en_comprobar_farkle  : std_logic;
-signal dado_pto                 : std_logic_vector(2 downto 0);
+signal clk, reset, en_calcula   : std_logic;
+signal dados                    : std_logic_vector(17 downto 0);
 
 --Outputs
 signal error, ready_puntuacion, farkle_ok  : std_logic;
@@ -48,91 +47,40 @@ begin
     reset <= '1';
     wait for 100 ns;
     reset <= '0';
-    
+    wait for 100 ns;
     --Viene la siguiente secuencia de dados 110501
     -- Debe dar como puntuacion 1050 ptos
     --error=0
-    en_calcula<='1';
-    dado_pto<="001";        
-    wait for clk_period; 
-     
-    dado_pto<="001";
-    wait for clk_period; 
-    
-    dado_pto<="000";
-    wait for clk_period; 
-    
-
-    dado_pto<="101";
-    wait for clk_period;  
-    
-    dado_pto<="000";
-    wait for clk_period; 
-       
- 
-    dado_pto<="001";
-    wait for clk_period;
     en_calcula<='0';
-    wait for clk_period;
+    
+    en_calcula<='1';
+    dados<="001001000101000001";        
+    wait for 100 ns;  
+    en_calcula<='0';
+    wait for 100 ns;
 
     --La persona ha seleccionado los dados mal, ha bajado los 2 primeros switches -66XXXX-
     --661050 con puntuacion de 150 aunque luego hay que ver desde la maquina de estados que no salga puntuacion sino un error
     --error=1, farkle_ok=0, en_suma_ronda=0
     
     en_calcula<='1'; 
-    dado_pto<="110";        
-    wait for clk_period; 
-    
-
-    dado_pto<="110";
-    wait for clk_period; 
-    
-
-    dado_pto<="001";
-    wait for clk_period; 
-    
-
-    dado_pto<="000";
-    wait for clk_period; 
-    
-
-    dado_pto<="101";
-    wait for clk_period; 
-    
-
-    dado_pto<="000";
-    wait for clk_period; 
-    
+    dados<="110110001000101000";        
+     wait for 100 ns;  
     en_calcula<='0';
-    wait for clk_period; 
+    wait for 100 ns; 
+ 
     
     --Secuencia con farkle 643226 salida farkle_ok
     --error=0, farkle_ok=1, en_suma_ronda=0 
     --La persona no ha seleccionado pero deberia salir farkle
     
     en_calcula<='1';
-    dado_pto<="110";        
-    wait for clk_period; 
-    
-
-    dado_pto<="100";
-    wait for clk_period; 
-    
-    dado_pto<="011";
-    wait for clk_period;  
-    
-
-    dado_pto<="010";
-    wait for clk_period; 
-    
-
-    dado_pto<="010";
-    wait for clk_period; 
-    
-
-    dado_pto<="110";
-    wait for clk_period; 
+    dados<="110100011010010110";        
+     wait for 100 ns;  
     en_calcula<='0';
+    wait for 100 ns;
+    
+
     
     wait;
     
@@ -143,8 +91,7 @@ i_Puntuacion: Puntuacion
   Port map (clk     => clk, 
             reset   => reset,
             en_calcula=> en_calcula,
-            dado_pto=> dado_pto, 
-            en_comprobar_farkle=> en_comprobar_farkle, 
+            dados=> dados, 
             ptos    => ptos, 
             error   => error,
             ready_puntuacion=> ready_puntuacion, 
