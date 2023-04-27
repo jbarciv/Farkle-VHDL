9baac6e8d9a70d8a565105cbf8b4b69765cf0e6b
estrella <estrella.dlopez@alumnos.upm.es>
2023-04-27 15:21:46 +0200

    fsm puntuacion

--- Puntuacion.vhd
+++ Puntuacion.vhd
@@ -3,16 +3,15 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
 
-entity Puntuacion is
-  Port (clk     : in std_logic; 
-        reset   : in std_logic;
-        dado_pto: in std_logic_vector(2 downto 0);
-        ptos    : out std_logic_vector(13 downto 0);
-        error   : out std_logic;
-        farkle_ok  : out std_logic;
-        dado_valido : in std_logic; --Viene del bloque select dados dura solo 1 ciclo de reloj, o mejor que dure todos los ciclos de reloj. 
-        en_suma_ronda: out std_logic;
-        ready_puntuacion: out std_logic
+entity Puntuacion is --decirle a chema cuantos dados tiene que mostrar en la siguiente tirada
+  Port (clk                 : in std_logic;
+        reset               : in std_logic;
+        en_calcula          : in std_logic;
+        dados               : in std_logic_vector(17 downto 0);
+        ptos                : out std_logic_vector(13 downto 0);
+        error               : out std_logic;
+        ready_puntuacion    : out std_logic;
+        farkle_ok           : out std_logic 
         );
 end Puntuacion;
 
@@ -21,37 +20,88 @@ architecture Behavioral of Puntuacion is
 signal count_0,count_1, count_2, count_3, count_4, count_5, count_6 : integer range 0 to 6;
 signal ptos_tirada         : integer range 0 to 10000;
 signal ptos_1,ptos_2,ptos_3,ptos_4,ptos_5,ptos_6 : integer range 0 to 3000;
-
+signal ptos_i   : std_logic_vector(13 downto 0);
 --Co
-signal cnt_dados    : unsigned(2 downto 0);
-signal ovf_dados    : std_logic;
---signal en_dados     : std_logic;
+signal cnt_dados    : unsigned(2 downto 0); 
+signal ovf_dados    : std_logic; 
+signal flag_cnt     : std_logic; 
+signal flag_aux     : std_logic; 
+signal dado_pto     : std_logic_vector(2 downto 0);
 
-----FSM
---type state_t is (S_WAIT, S_PUNTUACION, S_FARKLE);
---signal state    : state_t; 
+--FSM 
+type State_t is (S_ESPERA, S_CALCULANDO, S_CALCULADO);
+signal STATE : State_t; 
+
 
 begin
+--Proceso de actualizacion de estados, a partir de las entradas y del estado actual 
+process(clk,reset)
+begin
+    if reset='1' then 
+        STATE<= S_ESPERA; 
+    elsif clk'event and clk='1' then 
+        case STATE is 
+            when S_ESPERA=> 
+                if en_calcula='1' then 
+                    STATE<=S_CALCULANDO;
+                end if;                     
+            when S_CALCULANDO=>
+                if flag_cnt='1' then 
+                    STATE<=S_CALCULADO;
+                end if;
+            when S_CALCULADO=>
+                ready_puntuacion<='1';
+                if en_calcula='0' then 
+                    STATE<=S_ESPERA;
+                end if;
+            when others=>
+        end case;
+    end if;
+end process;
 
-
-
+--Guardamos el valor en un registro
+process(clk,reset)
+begin 
+    if reset='1' then 
+        ptos_i<=(others=>'0');
+    elsif clk'event and clk='1' then 
+        if STATE=S_ESPERA then 
+            ptos_i<=(others=>'0');
+        elsif STATE=S_CALCULADO then 
+            ptos_i<=std_logic_vector(TO_UNSIGNED(ptos_tirada,14));
+        end if;
+    end if;
+end process;             
+                
 --Contador dados que entran --Hay que utilizar la variable count_0, count_1, etc
 process(clk,reset)
 begin
     if reset='1' then 
-        cnt_dados<="000";
-    elsif clk'event and clk='1' then
-        if cnt_dados=6 and dado_valido='0' then 
-                cnt_dados<="000";
-        end if;        
-        if dado_valido='1' then  
-           cnt_dados<=cnt_dados+1;
+        cnt_dados<="000"; 
+    elsif clk'event and clk='1' then 
+        if STATE=S_CALCULANDO then 
+            if cnt_dados=5 then 
+                cnt_dados<="000"; 
+                flag_cnt<='1'; 
+            else
+               cnt_dados <= cnt_dados + 1; 
+            end if;
         end if;
-    end if;
+   end if;
 end process; 
 
-ovf_dados<='1' when cnt_dados="110" and dado_valido='1' else '0'; --Senal que conecte con ovf
+------------------------------------
+-- SEPARADOR DADOS 
+------------------------------------
 
+with cnt_dados select 
+ dado_pto<=dados(17 downto 15) when "000", --0
+           dados(14 downto 12) when "001", --1
+           dados(11 downto 9) when "010", --2
+           dados(8 downto 6) when "011", --3
+           dados(5 downto 3) when "100", --4
+           dados(2 downto 0) when "101", --5
+           "---" when others; 
 --Logica puntuacion
 process(clk,reset)
 begin 
@@ -64,7 +114,8 @@ begin
         count_6<=0;
         count_0<=0;
     elsif(clk'event and clk='1') then 
-        if ovf_dados='0' then 
+        if STATE=S_CALCULANDO then 
+        --if (ovf_dados='0' and (en_calcula='1' or en_comprobar_farkle='1')) then
             case dado_pto is 
                 when "001" =>
                     count_1<=count_1+1;
@@ -83,7 +134,7 @@ begin
                 when others =>
                     --No deberia pasar nada
             end case;
-        else 
+        elsif STATE=S_ESPERA then 
             count_1<=0; --Estado de S_RESET y S_COMPROBACION (pulso dados validos en gral)estado de entrada de nuevo valor. Mealy
             count_2<=0;                --S_COMPROBACION -> S_ RESET  senal de dado valido 
             count_3<=0;         -- Contador de 0 a 5 para comprobar que se han contado todos los dados
@@ -99,7 +150,7 @@ end process;
 --Puntuacion/Me han metido un dado mal
 -------------------------------------------------------
 with count_1 select 
-    ptos_1<=    100 when 1, 
+    ptos_1<=   100 when 1, 
                200 when 2,
                1000 when 3, 
                1000 when 4,
@@ -138,39 +189,23 @@ with count_5 select
 
 with count_6 select 
     ptos_6<=    600 when 3, 
-               1000 when 4,
+               1000 when 4, 
                2000 when 5, 
                3000 when 6, 
                0 when others;       
            
 
 ptos_tirada<=ptos_1+ptos_2+ptos_3+ptos_4+ptos_5+ptos_6;
-ptos<=std_logic_vector(TO_UNSIGNED(ptos_tirada,14)); --when ovf_dados='1' else
-      --(others=>'0');
-en_suma_ronda<='1' when (ptos_tirada/=0 and ovf_dados='1') else '0';
 
-error<='1' when ((count_2=1 or count_2=2) or
+
+error<='1' when ((count_2=1 or count_2=2) or 
                   (count_3=1 or count_3=2) or 
                   (count_4=1 or count_4=2) or 
-                  (count_6=1 or count_6=2)) and ovf_dados='1' else '0';
+                  (count_6=1 or count_6=2)) and STATE=S_CALCULADO else '0';
 
 
-
-
-
--------------------------------------------------------
---FARKLE
--------------------------------------------------------
-farkle_ok<='1' when ptos_tirada=0 and ovf_dados='1' else '0';
-
-------------------
---COMPROBAR FARKLE
-------------------
-
-
-
-
-
+--Hay farkle
+farkle_ok <= '1' when (count_1 = 0 and count_5 = 0 and count_2 < 3 and count_3 < 3 and count_4 < 3 and count_6 < 3 and STATE=S_CALCULADO) else '0';     
 
 
 end Behavioral;
