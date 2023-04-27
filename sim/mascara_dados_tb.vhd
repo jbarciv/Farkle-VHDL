8cd28a4f7ba4c3ed36ec20c05aff12fb47cacd40
chema <chemabc@gmail.com>
2023-04-27 16:32:56 +0200

    minor

--- sim/mascara_dados_tb.vhd
+++ sim/mascara_dados_tb.vhd
@@ -12,11 +12,10 @@ architecture behavioral of mascara_dados_tb is
   signal sw           : std_logic_vector(5 downto 0)  := "000000";
   signal dados        : std_logic_vector(17 downto 0) := "011011001110100011";  -- dado = 331643  
   signal en_select    : std_logic;
-  signal dados_s      : std_logic_vector(17 downto 0); 
-  signal ready_select : std_logic
 
   --Outputs
-  signal dado_pto: std_logic_vector(2 downto 0);
+  signal dados_s      : std_logic_vector(17 downto 0); 
+  signal ready_select : std_logic;
 
   -- Clock period definitions
   constant clk_period : time := 8 ns;
