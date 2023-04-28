c659e6ed6175694c8e6aca7348e60165fc2d3832
estrelii <96571158+estrelii@users.noreply.github.com>
2023-04-28 11:27:32 +0200

    scroll 2804

--- src/scroll.vhd
+++ src/scroll.vhd
@@ -1,115 +1,76 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
-entity scroll_tb is
-end scroll_tb;
-
-architecture Behavioral of scroll_tb is
-
-component scroll is
-    Port (  clk         : in std_logic;
-            reset       : in std_logic;
-            dados       : in std_logic_vector(20 downto 0);
-            en_refresh  : in std_logic;
-            num_dados_mostrar: in std_logic_vector(2 downto 0);
-            dados_s     : out std_logic_vector(20 downto 0)
+entity scroll is
+    Port (  clk                 : in std_logic;
+            reset               : in std_logic;
+            dados               : in std_logic_vector(20 downto 0);
+            en_refresh          : in std_logic;
+            num_dados_mostrar   : in std_logic_vector(2 downto 0);
+            dados_s             : out std_logic_vector(20 downto 0)
             
     );
-end component;
+end scroll;
 
-signal clk,reset, en_refresh : std_logic;
-signal num_dados_mostrar: std_logic_vector(2 downto 0);
-signal dados        : std_logic_vector(20 downto 0);
-signal dados_s      : std_logic_vector(20 downto 0);
+architecture Behavioral of scroll is
 
-constant clk_period : time := 8 ns;
+-- Senales dados
+signal dados_i: std_logic_vector(20 downto 0);
+-- Señales divisor de frecuencia
+constant maxcount   : integer := 125*10**3;   -- cambiar a 125000000 para probar en la placa f�sica
+signal count        : integer range 0 to maxcount-1;
+signal enable_1s    : std_logic;
 
 begin
--- Generacion del reloj
-    
-    clk_proc: process
-        begin
-        clk <= '1';
-        wait for clk_period/2;
-        clk<= '0';
-        wait for clk_period/2;
-    end process;
 
--- Proceso generacion de estimulos
+-- Dvisor de frecuencia 
 
- stim_proc: process
-   begin		
-      -- hold reset state for 100 ns.
-		reset <= '1';
-      wait for 10 ns;	
-		reset<= '0';
-      wait for 100 ns; 
-      
-      num_dados_mostrar<="001"; --1 dado
-      dados<="001111010011100101110";
-      en_refresh<='1'; 
-      wait for clk_period; 
-      en_refresh<='0';
-      wait for 10 ms; 
-      
-      num_dados_mostrar<="010"; --2 dados
-      dados<="001010111011100101110";
-      en_refresh<='1'; 
-      wait for clk_period; 
-      en_refresh<='0';
-      wait for 10 ms; 
-      
-      num_dados_mostrar<="011"; --3 dados
-      dados<="001010011111100101110";
-      en_refresh<='1'; 
-      wait for clk_period; 
-      en_refresh<='0';
-      wait for 10 ms; 
-      
-      num_dados_mostrar<="100"; --4 dados
-      dados<="001010011100111101110";
-      en_refresh<='1'; 
-      wait for clk_period; 
-      en_refresh<='0';
-      wait for 10 ms; 
-        
-      dados<="001010011100101111110";
-      num_dados_mostrar<="101"; --5 dados
-      en_refresh<='1'; 
-      wait for clk_period; 
-      en_refresh<='0';
-      
-      wait for 10 ms; 
+process(clk, reset)
+begin
+    if (reset = '1') then
+        count <= 0;
+    elsif (clk'event and clk = '1') then       
+            if(count = maxcount-1) then
+                count <= 0;
+            else 
+                count <= count + 1;
+            end if;
+    end if;    
+end process;      
 
-      
-      num_dados_mostrar<="110"; --6 dados
-      dados<="001010011100101110111";
-      en_refresh<='1'; 
-      wait for clk_period; 
-      en_refresh<='0';
-      wait;
-      
-      
-   end process;
+enable_1s <= '1' when(count = maxcount-1) else '0';
 
--- Instanciar componente 
+-- Desplazamiento
+    process(clk, reset)
+    begin
+        if (reset = '1') then
+            dados_i <= (others => '0');
+        elsif(clk'event and clk = '1') then
+            if en_refresh='1' then 
+                dados_i<=dados;
+            end if; 
+            if enable_1s='1' then 
+                case num_dados_mostrar is 
+                    when "101"=> 
+                        dados_i<=dados_i(17 downto 3)& dados_i(20 downto 18)&dados_i(2 downto 0);
+                    when "110"=>
+                        dados_i<= dados_i(17 downto 0) & dados_i(20 downto 18);
+                    when others=>
+                    
+                end case; 
+            end if;
+        end if;            
+    end process;  
 
-    CUT: scroll port map(
-        clk => clk,
-        reset => reset,
-        dados=>dados,
-        en_refresh => en_refresh,
-        num_dados_mostrar=>num_dados_mostrar,
-        dados_s=>dados_s
-        );
+with num_dados_mostrar select  
 
+dados_s <= dados_i when "101", 
+           dados_i when "110", 
+           "000000000000000000"& dados_i(20 downto 18) when "001", 
+           "000000000000000" & dados_i(20 downto 15) when "010", 
+           "000000000000" & dados_i(20 downto 12) when "011", 
+           "000000000" & dados_i(20 downto 9) when "100", 
+           "---------------------" when others;
+           
 end Behavioral;
