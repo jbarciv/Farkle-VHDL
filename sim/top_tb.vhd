068cd13aaef6f6ee8523526e5ddecc057a439e87
estrelii <estrella.dlopez@alumnos.upm.es>
2023-06-20 13:37:55 +0200

    hola

--- sim/top_tb.vhd
+++ sim/top_tb.vhd
@@ -8,7 +8,33 @@ end top_tb;
 
 architecture Behavioral of top_tb is
 
+clk         :std_logic;
+reset       :std_logic;
+tirar       :std_logic;
+sel         :std_logic;
+planta      :std_logic;
+switch      :std_logic_vector(5 downto 0);
+leds        : std_logic_vector(7 downto 0);
+segmentos   : std_logic_vector(7 downto 0);
+selector    : std_logic_vector(3 downto 0)
+
 begin
+
+    
+
+
+
+i_TOP: entity work.top
+Port map     (clk         =>clk,      
+              reset       =>reset,     
+              tirar       =>tirar,     
+              sel         =>sel,       
+              planta      =>planta,    
+              switch      =>switch,    
+              leds        =>leds,      
+              segmentos   =>segmentos, 
+              selector    =>selector  
+              );
 
 
 
