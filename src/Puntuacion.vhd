0f8a8e6edb577392dc7709803ba9aa3eeb1599b5
Josep Mª Barberá Civera <chemabc@gmail.com>
2023-06-26 11:35:22 +0200

    Update Puntuacion.vhd

--- src/Puntuacion.vhd
+++ src/Puntuacion.vhd
@@ -7,13 +7,15 @@ entity Puntuacion is
   Port (clk                 : in std_logic;
         reset               : in std_logic;
         en_calcula          : in std_logic;
-        dados               : in std_logic_vector(20 downto 0);
+        en_calcula_farkle   : in std_logic;
+        dados_mascara       : in std_logic_vector(20 downto 0);
+        dados_farkle        : in std_logic_vector(20 downto 0);
         ptos                : out std_logic_vector(13 downto 0);
         error_s             : out std_logic;
         flag_puntuacion     : out std_logic;
         farkle_s            : out std_logic; 
         dados_sel           : out std_logic_vector(2 downto 0);
-        flag_sel            : in std_logic
+        dados_mostrar       : in std_logic_vector(2 downto 0)         
         );
 end Puntuacion;
 
@@ -22,19 +24,19 @@ architecture Behavioral of Puntuacion is
 signal count_1, count_2, count_3, count_4, count_5, count_6 : unsigned(2 downto 0);
 signal ptos_tirada         : integer range 0 to 10000;
 signal ptos_1,ptos_2,ptos_3,ptos_4,ptos_5,ptos_6 : integer range 0 to 3000;
-signal ptos_i   : std_logic_vector(13 downto 0);
+--signal ptos_i   : std_logic_vector(13 downto 0);
 --Co
 signal cnt_dados    : unsigned(2 downto 0); 
-signal ovf_dados    : std_logic; 
-signal flag_cnt     : std_logic; 
+--signal ovf_dados    : std_logic; 
+signal flag_cnt, flag_error,flag_farkle, flag_calcula_farkle, flag_calcula     : std_logic; 
 signal dado_pto     : std_logic_vector(2 downto 0);
 signal error,farkle_ok        : std_logic;
-signal count_dados           : std_logic_vector(2 downto 0);
+--signal count_dados           : std_logic_vector(2 downto 0);
+signal dados       : std_logic_vector(20 downto 0);
 
 --FSM 
-type State_t is (S_ESPERA, S_CALCULANDO, S_CALCULADO);
+type State_t is (S_ESPERA, S_COMPROBANDO_FARKLE, S_CALCULANDO, S_CALCULADO);
 signal STATE : State_t; 
-
 
 
 begin
@@ -42,40 +44,86 @@ begin
 process(clk,reset)
 begin
     if reset='1' then 
-        STATE<= S_ESPERA; 
-        ptos<=(others=>'0');
-        dados_sel<="000";
+        STATE<= S_ESPERA;
+        flag_error <= '0';
+        flag_farkle <= '0';
+        flag_calcula_farkle <= '0';
+        flag_calcula <= '0';
+        flag_puntuacion<='0';
+        dados <= (others=>'0');
+        
     elsif clk'event and clk='1' then 
         case STATE is 
             when S_ESPERA=> 
                 error_s<='0';
-                farkle_s<='0';
-                ptos<=(others=>'0');
+                farkle_s<='0';          
                 if (en_calcula='1') then 
                     STATE<=S_CALCULANDO;
-                end if;                     
-            when S_CALCULANDO=>
-                flag_puntuacion<='1';
+                    dados <= dados_mascara;
+                    flag_calcula <= '1';
+                elsif (en_calcula_farkle = '1') then
+                    STATE <= S_COMPROBANDO_FARKLE;
+                    dados <= dados_farkle;
+                    flag_calcula_farkle <= '1';
+                end if;
+            when S_COMPROBANDO_FARKLE=>
+                --flag_puntuacion<='1';
                 if flag_cnt='1' then 
                     STATE<=S_CALCULADO;
+                    flag_puntuacion<='1';
+                    if(farkle_ok='1' and flag_calcula_farkle = '1') then 
+                        farkle_s<='1';
+                        flag_farkle<='1';
+                    end if;
+                end if;
+                                 
+            when S_CALCULANDO=>
+                --flag_puntuacion<='1';
+                if flag_cnt='1' then 
+                    STATE<=S_CALCULADO;
+                    flag_puntuacion<='1';
+                    if(error='1' and flag_calcula = '1') then 
+                        error_s<='1';
+                        flag_error<='1';
+                    
+                    end if;
                 end if;
             when S_CALCULADO=>
-                flag_puntuacion<='0';
-                if(error='1' and flag_sel='1') then 
-                    error_s<='1';
-                   
-                elsif(farkle_ok='1') then 
-                    farkle_s<='1';
-                end if; 
-                ptos<=std_logic_vector(TO_UNSIGNED(ptos_tirada,14));
-                dados_sel<=count_dados;    
-                if(en_calcula='0') then
+                 
+--                if(farkle_ok='1' and flag_calcula_farkle = '1') then 
+--                    farkle_s<='1';
+--                    flag_farkle<='1';
+--                elsif(error='1' and flag_calcula = '1') then 
+--                    error_s<='1';
+--                    flag_error<='1';
+                --end if; 
+                --ptos<=std_logic_vector(TO_UNSIGNED(ptos_tirada,14));  
+                if((en_calcula_farkle='0' and flag_farkle='1') or (en_calcula='0' and flag_error='1') or (flag_farkle='0' and flag_error='0' and en_calcula='0')) then
                     STATE<=S_ESPERA;
+                    flag_farkle<='0';
+                    flag_error<='0'; 
+                    flag_calcula_farkle <= '0';
+                    flag_calcula <= '0';
+                    flag_puntuacion<='0';
                 end if;
             when others=>
         end case;
     end if;
-end process;   
+end process;  
+
+process(clk,reset)
+begin 
+    if reset='1' then 
+        dados_sel<=(others=>'0');
+        ptos<=(others=>'0');
+    elsif clk'event and clk='1' then 
+        if STATE=S_CALCULADO then 
+        ptos <= std_logic_vector(TO_UNSIGNED(ptos_1+ptos_2+ptos_3+ptos_4+ptos_5+ptos_6,14)); 
+        dados_sel <= std_logic_vector(count_1+count_2+count_3+count_4+count_5+count_6);
+        end if;
+    end if;
+end process;        
+         
                 
 --Contador dados que entran --Hay que utilizar la variable , count_1, etc
 process(clk,reset)
@@ -83,12 +131,21 @@ begin
     if reset='1' then 
         cnt_dados<="000"; 
     elsif clk'event and clk='1' then 
-        if STATE=S_CALCULANDO and flag_cnt='0' then 
-            if cnt_dados=6 then 
-                cnt_dados<="000"; 
-                flag_cnt<='1'; 
-            else
-               cnt_dados <= cnt_dados + 1; 
+        if(flag_cnt='0') then 
+            if (STATE=S_COMPROBANDO_FARKLE)  then 
+                if cnt_dados=unsigned(dados_mostrar) then 
+                    cnt_dados<="000"; 
+                    flag_cnt<='1'; 
+                else
+                   cnt_dados <= cnt_dados + 1;
+                end if;
+            elsif (STATE=S_CALCULANDO) then 
+                if cnt_dados="110" then 
+                    cnt_dados<="000"; 
+                    flag_cnt<='1'; 
+                else
+                   cnt_dados <= cnt_dados + 1; 
+                end if;
             end if;
         elsif STATE=S_ESPERA then 
             flag_cnt<='0';
@@ -104,7 +161,7 @@ begin
     if reset='1' then 
         dado_pto<=(others=>'0');
     elsif clk'event and clk='1' then 
-        if state=S_CALCULANDO and flag_cnt='0' then 
+        if (STATE=S_CALCULANDO or STATE=S_COMPROBANDO_FARKLE)  and flag_cnt='0' then 
             case cnt_dados is 
                 when "000"=> 
                     dado_pto<=dados(20 downto 18); 
@@ -141,7 +198,7 @@ begin
         count_5<=(others=>'0');
         count_6<=(others=>'0');
     elsif(clk'event and clk='1') then 
-        if STATE=S_CALCULANDO then 
+        if STATE=S_CALCULANDO or STATE=S_COMPROBANDO_FARKLE then 
         --if (ovf_dados='0' and (en_calcula='1' or en_comprobar_farkle='1')) then
             case dado_pto is 
                 when "001" =>
@@ -171,16 +228,16 @@ begin
     end if;
 end process;
 
-process
-begin 
-    if reset='1' then 
-        count_dados<=(others=>'0');
-    elsif clk'event and clk='1' then
-        if STATE=S_CALCULADO then 
-            count_dados<=std_logic_vector(count_1+count_2+count_3+count_4+count_5+count_6);
-        end if;
-    end if;
-end process;     
+--process(clk, reset)
+--begin 
+--    if reset='1' then 
+--        count_dados<=(others=>'0');
+--    elsif clk'event and clk='1' then
+--        if STATE=S_CALCULADO then 
+--            count_dados<=std_logic_vector(count_1+count_2+count_3+count_4+count_5+count_6);
+--        end if;
+--    end if;
+--end process;     
 
 
 
@@ -230,18 +287,15 @@ with count_6 select
                0 when others;       
            
 
-ptos_tirada<=ptos_1+ptos_2+ptos_3+ptos_4+ptos_5+ptos_6;
-
-
 error<='1' when ((count_2=1 or count_2=2) or 
                   (count_3=1 or count_3=2) or 
                   (count_4=1 or count_4=2) or 
-                  (count_6=1 or count_6=2)) and STATE=S_CALCULADO else '0';
+                  (count_6=1 or count_6=2)) else '0';
 
 
 
 --Hay farkle
-farkle_ok <= '1' when (count_1 = 0 and count_5 = 0 and count_2 < 3 and count_3 < 3 and count_4 < 3 and count_6 < 3 and STATE=S_CALCULADO) else '0';     
+farkle_ok <= '1' when (count_1 = 0 and count_5 = 0 and count_2 < 3 and count_3 < 3 and count_4 < 3 and count_6 < 3) else '0';     
 
 
 end Behavioral;
